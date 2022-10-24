fb57b375dc44171838aaba7cc560dbbb6b4cd10a
Mateus-Belfort <mateusaguia96@gmail.com>
2022-10-24 16:33:25 -0300

    Atualizacao_Print

--- assets/index.js
+++ assets/index.js
@@ -1,4 +1,15 @@
 var btn = document.querySelector("button");
+const printPage = () => {
+    const printFrame = document.createElement('iframe');
+    printFrame.style.display  ='none';
+    printFrame.src = 'index.html';
+
+    document.body.appendChild(printFrame);
+
+    printFrame.contentWindow.focus();
+    printFrame.contentWindow.print();
+}
+
 btn.onclick = () => {
     window.print();
 }
